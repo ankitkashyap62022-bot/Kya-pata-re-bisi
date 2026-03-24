a1d8d342dede1bb5bf0b55e782240f1ec44c2890
ragini19854-prog <ragini.19854@gmail.com>
2026-03-25 01:01:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ── Git / updates ──────────────────────────────────────────────────────────────
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/CertifiedCoders/.....")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ragini19854-prog/Kya-pata-re-bisi")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN")  # needed if repo is private
 
