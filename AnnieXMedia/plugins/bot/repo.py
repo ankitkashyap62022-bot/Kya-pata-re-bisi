d79c11344df8bcb122027f08effa817a904a63da
ragini19854-prog <ragini.19854@gmail.com>
2026-03-25 00:22:01 +0530

    Update repo.py

--- AnnieXMedia/plugins/bot/repo.py
+++ AnnieXMedia/plugins/bot/repo.py
@@ -5,15 +5,11 @@ from AnnieXMedia import app
 from config import BOT_USERNAME
 
 repo_caption = """**
-🚀 ᴄʟᴏɴᴇ ᴀɴᴅ ᴅᴇᴘʟᴏʏ – ᴄᴇʀᴛɪꜰɪᴇᴅ ᴄᴏᴅᴇʀꜱ ʀᴇᴘᴏ 🚀
-
-➤ ᴅᴇᴘʟᴏʏ ᴇᴀsɪʟʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴇʀʀᴏʀꜱ  
-➤ ɴᴏ ʜᴇʀᴏᴋᴜ ʙᴀɴ ɪꜱꜱᴜᴇ  
-➤ ɴᴏ ɪᴅ ʙᴀɴ ɪꜱꜱᴜᴇ  
-➤ ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏꜱ  
-➤ ʀᴜɴ 24/7 ʟᴀɢ ꜰʀᴇᴇ
-
-ɪꜰ ʏᴏᴜ ꜰᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ, ꜱᴇɴᴅ ꜱꜱ ɪɴ ꜱᴜᴘᴘᴏʀᴛ
+ʜᴇʏ ʙᴀʙʏ 👋
+ᴛʜᴇ ᴄᴏᴅᴇ ғᴏʀ ᴛʜɪs ʙᴏᴛ ɪs ᴘʀɪᴠᴀᴛᴇ 🔒
+ɪᴛ’s ɴᴏᴛ ᴀᴠᴀɪʟᴀʙʟᴇ ᴛᴏ sʜᴀʀᴇ ʀɪɢʜᴛ ɴᴏᴡ ✨
+ᴘʟᴇᴀsᴇ ᴅᴏɴ’ᴛ ᴀsᴋ ғᴏʀ ᴛʜᴇ ʀᴇᴘᴏ 🙌
+ᴛʜᴀɴᴋs ғᴏʀ ᴜɴᴅᴇʀsᴛᴀɴᴅɪɴɢ 🤍
 **"""
 
 @app.on_message(filters.command("repo"))
@@ -21,12 +17,11 @@ async def show_repo(_, msg):
     buttons = [
         [InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
         [
-            InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/CertifiedCoder"),
-            InlineKeyboardButton("💬 ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/CertifiedCodes")
+            InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/II_YOUR_MADARA_II"),
+            InlineKeyboardButton("💬 ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/MADARA_DEFAULTER_ABOUT")
         ],
         [
-            InlineKeyboardButton("🛠️ ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/CertifiedCoders"),
-            InlineKeyboardButton("🎵 ɢɪᴛʜᴜʙ", url="https://github.com/CertifiedCoders/AnnieXMusic")
+            InlineKeyboardButton("🛠️ ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/FRIEND_ZONE_BY_MADARA")
         ]
     ]
 
